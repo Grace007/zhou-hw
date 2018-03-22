fde154d6399469a383f2fb17d7ce130ab1fd5ba1
821132246@qq.com <15809713216run>
2018-03-22 11:43:01 +0800

    初始导入

--- src/main/java/com/demo/nio/TraditionalServer.java
+++ src/main/java/com/demo/nio/TraditionalServer.java
@@ -24,6 +24,7 @@ import java.net.Socket;
  */
 public class TraditionalServer {
     public static void main(String[] args) throws Exception {
+        // sas
         ServerSocket serverSocket = new ServerSocket(2000);
         System.out.println("serverSocket = " +serverSocket.getLocalSocketAddress());
         while (true){
